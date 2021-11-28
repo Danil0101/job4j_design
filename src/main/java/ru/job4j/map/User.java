32a3a43e580a0c0e15ed73a33be1598b1da58b2f
Danil Selyutin <selyutin.d.g@gmail.com>
2021-11-28 09:14:29 +0300

    8. Реализовать собственную структуру данных - HashMap [#1008 #233264]

--- src/main/java/ru/job4j/map/User.java
+++ src/main/java/ru/job4j/map/User.java
@@ -13,30 +13,6 @@ public class User {
         this.birthday = birthday;
     }
 
-    public static void main(String[] args) {
-        User user1 = new User("John", 2,
-                new GregorianCalendar(2000, Calendar.JANUARY, 1));
-        User user2 = new User("John", 2,
-                new GregorianCalendar(2000, Calendar.JANUARY, 1));
-        Map<User, Object> map = new HashMap<>();
-        map.put(user1, new Object());
-        map.put(user2, new Object());
-        System.out.println(map);
-
-        int hashU1 = user1.hashCode() ^ user1.hashCode() >>> 16;
-        int hashU2 = user2.hashCode() ^ user2.hashCode() >>> 16;
-        int index1 = hashU1 & 15;
-        int index2 = hashU2 & 15;
-        System.out.println("hashCode1: "
-                + user1.hashCode()
-                + " hash1: " + hashU1
-                + " index1: " + index1);
-        System.out.println("hashCode2: "
-                + user2.hashCode()
-                + " hash2: " + hashU2
-                + " index2: " + index2);
-    }
-
     @Override
     public boolean equals(Object o) {
         if (this == o) {
