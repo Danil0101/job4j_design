d716bc2f2265dea79cbbbb690924eae36f021a4a
Danil Selyutin <selyutin.d.g@gmail.com>
2021-12-09 08:42:47 +0300

    2. Статистика по коллекции. [#45889 #237366]

--- src/main/java/ru/job4j/question/Analize.java
+++ src/main/java/ru/job4j/question/Analize.java
@@ -1,20 +1,26 @@
 package ru.job4j.question;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
 
 public class Analize {
     public static Info diff(Set<User> previous, Set<User> current) {
-    Info result = new Info(current.size(), 0, previous.size());
-        for (User curUser : current) {
-            for (User preUser : previous) {
-                if (curUser.getId() == preUser.getId()) {
-                    if (!Objects.equals(curUser.getName(), preUser.getName())) {
-                        result.setChanged(result.getChanged() + 1);
-                    }
-                    result.setAdded(result.getAdded() - 1);
-                    result.setDeleted(result.getDeleted() - 1);
+        Info result = new Info(0, 0, previous.size());
+        Map<Integer, User> map = new HashMap<>();
+        for (User user : previous) {
+            map.put(user.getId(), user);
+        }
+        for (User user : current) {
+            User prevUser = map.put(user.getId(), user);
+            if (prevUser == null) {
+                result.setAdded(result.getAdded() + 1);
+            } else {
+                if (!Objects.equals(user.getName(), prevUser.getName())) {
+                    result.setChanged(result.getChanged() + 1);
                 }
+                result.setDeleted(result.getDeleted() - 1);
             }
         }
         return result;
