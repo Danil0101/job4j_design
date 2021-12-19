a090959f3f879c84c535a9fd2c159b2d366b3f30
Danil Selyutin <selyutin.d.g@gmail.com>
2021-12-19 13:11:43 +0300

    5. Валидация параметров запуска. [#246865 #241665]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -15,7 +15,7 @@ public class Search {
         }
         Path start = Paths.get(args[0]);
         if (!start.toFile().exists() || !start.toFile().isDirectory()) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("Directory not found");
         }
         search(start, p -> p.toFile().getName().endsWith(args[1])).forEach(System.out::println);
     }
